e59f0064bd4bddb701e512074d9b52f048eed681
ibra <ibrahim.balta@edu.univ-fcomte.fr>
2024-11-08 08:59:34 +0100

    étape 0

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Bienvenue chez Quantic GP</h1>
+    <router-link to="/reservation">
+      <button>Réserver un Billet</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
+
+<style scoped>
+/* Styles basiques, si nécessaire */
+</style>

91cc8bf0707fbf62d5f945c48accc9b8bda8bdfc
VUILLIN Thomas <144356846+vuillin@users.noreply.github.com>
2024-11-08 16:55:05 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,31 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/HomeView.vue';
 import Reservation from '../views/ReservationView.vue';
+import FAQ from '../views/FAQView.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+
     {
       path: '/',
       name: 'Home',
       component: Home,
     },
+
     {
       path: '/reservation',
       name: 'Reservation',
       component: Reservation,
     },
+
+    {
+      path: '/faq',
+      name: 'FAQ',
+      component: FAQ,
+    },
+
   ],
 });

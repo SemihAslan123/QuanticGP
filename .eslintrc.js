e8f7e7d1739c9e5e2843d93858e7e5ffafdbb6fc
ibra <ibrahim.balta@edu.univ-fcomte.fr>
2024-11-18 11:27:56 +0100

    vue 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
+  'extends': [
+    'plugin:vue/essential',
     'eslint:recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',

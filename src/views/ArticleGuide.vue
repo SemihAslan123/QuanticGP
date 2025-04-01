512be1574b2e9ce75ba8dea731fc2d552166d1d7
VUILLIN Thomas <144356846+vuillin@users.noreply.github.com>
2025-04-01 09:34:15 +0200

    Update ArticleGuide.vue

--- src/views/ArticleGuide.vue
+++ src/views/ArticleGuide.vue
@@ -3,6 +3,364 @@
     <div>
 
         <h1 class="titreArticle">Le guide pratique du Quantic GP</h1>
+
+        <p class="sousTitreArticle">Assister à une course sur circuit en tant que spectateur, 
+            c'est plonger dans une expérience inoubliable. Mais comment en profiter pleinement et ne rien oublier ? 
+            Découvrez tous nos conseils et les règles du championnat dans ce guide pratique.
+
+            <br> <br>
+
+        </p>
+
+        <p class="sousTitreArticlebis">🗞️ - Article (5 minutes de lecture)</p>
+
+        <br>
+
+        <img src="/assets/homePage/articleGuide/guidePratique_image1.jpg" alt="image_soleil" class="imagesArticle2">
+
+        <hr class="hr1">
+
+
+        <div class="blocTexte1Container">
+
+            <p class="blocTexte1" style="text-align: center;">
+                Que vous assistiez à votre tout premier Grand Prix ou que vous soyez un habitué des circuits, 
+                il est toujours utile de vérifier le contenu de votre sac à dos. Un bon équipement est essentiel 
+                pour profiter pleinement de l'événement. Passons en revue tout ce qu'il vous faut pour être parfaitement préparé le <b>jour J</b>.
+            </p>
+
+        </div>
+
+
+        <h1 class="titreBloc">La météo</h1>
+
+        <div class="blocTexte1Container">
+            <p class="blocTexte1">Sur un circuit, vous avez plusieurs ennemis à considérer : <i><b>le soleil, la chaleur, le froid et la pluie</b></i>.
+                <br>Pourquoi distinguer ces éléments ? Parce qu'il peut faire chaud sans soleil, froid sans pluie, et inversement. 
+                Vous devez donc anticiper toutes les éventualités pour être à l’aise pendant tout le Grand Prix.</p>
+        </div>
+
+        <h1 class="sousTitreBloc">1. Le soleil et la chaleur</h1>
+
+        <div class="blocTexte1Container">
+
+            <p class="blocTexte1">Si le soleil garantit généralement un week-end au sec, il peut aussi s’avérer redoutable. 
+                En l’absence de protection adaptée, vous risquez rapidement des coups de soleil importants. 
+                Même sous un ciel voilé, les UV peuvent atteindre votre peau et provoquer des brûlures. De plus, si vous êtes installé sur des tribunes en plein air, 
+                sans ombre ni courant d’air, les fortes chaleurs peuvent rapidement devenir éprouvantes. Passer toute une journée sous un soleil estival 
+                peut non seulement vous fatiguer considérablement, mais aussi vous déshydrater. Et bien sûr, la transpiration sera au rendez-vous.
+            </p>
+
+        </div>
+
+                <br><br>
+
+                <div>
+                <img src="/assets/homePage/articleGuide/soleil_1.jpg" alt="image_soleil" class="imagesArticle photoHorizontal">
+                <img src="/assets/homePage/articleGuide/soleil_3.jpeg" alt="image_soleil" class="imagesArticle photoHorizontal">
+                <img src="/assets/homePage/articleGuide/soleil_2.jpg" alt="image_soleil" class="imagesArticle">
+                </div>
+
+                <br><br><br>
+
+        <div class="blocTexte1Container">        
+
+            <p class="blocTexte1">
+
+                <b><span class="listeArticleTitre">1.1 Comment se protéger efficacement du soleil et de la chaleur ?</span></b>
+
+                <br><br>
+
+                <b>A] <i class="fas fa-tshirt"></i> Adoptez des vêtements adaptés</b>
+
+                <br><br>
+
+                Privilégiez des vêtements légers et de couleur claire, qui réfléchissent les rayons du soleil. 
+                Une chemise en lin ou en coton très légère, à manches longues, vous permettra de vous protéger tout en restant au frais. 
+                De même, optez pour un pantalon léger plutôt qu’un short, qui expose davantage votre peau. Une casquette  est aussi une bonne idée mais veillez à ce 
+                qu’elle protège également vos oreilles. Si besoin, un chapeau en paille ou un bob pliable peut s’avérer plus efficace. 
+                Pensez à choisir un modèle peu encombrant et facile à ranger.
+
+                <br><br>
+
+                <b>B] <i class="fas fa-sun"></i> Utilisez de la crème solaire et protégez vos yeux</b>
+
+                <br><br>
+
+                Appliquez une crème solaire à indice élevé sur toutes les parties exposées de votre corps : 
+                visage, nuque, oreilles, bras, etc. Cela est indispensable, même si vous êtes habillé légèrement. De plus, des lunettes de soleil de bonne qualité sont 
+                essentielles pour éviter la fatigue visuelle et les irritations  après plusieurs heures sous un fort ensoleillement.
+
+                <br><br>
+
+                <b>C] <i class="fas fa-tint"></i> Restez hydraté et ventilez-vous</b>
+
+                <br><br>
+                
+                Emportez une bouteille d’eau avec vous. Elle ne servira pas uniquement à vous désaltérer : 
+                vous pourrez également l’utiliser pour humidifier vos vêtements ou rafraîchir votre visage. 
+                Pensez à la remplir régulièrement avec les points d’eau potable disponibles sur place. Un petit ventilateur à main ou un éventail 
+                peut s’avérer très utile. Si vous n’en avez pas, 
+                éventez vous avec le programme officiel qui vous sera remis sur place : il sera parfait pour créer 
+                un peu de brise lorsque l’air est immobile.
+                
+                <br><br>
+            </p>
+        </div>
+
+        <hr class="hr1">
+
+        <h1 class="sousTitreBloc">2. La pluie et le froid</h1>
+
+        <div class="blocTexte1Container">
+
+            <p class="blocTexte1">Même en plein été, assister à un Grand Prix ne garantit pas une météo clémente. 
+                Une course en juillet peut très bien être marquée par des averses ou des températures fraîches, 
+                surtout tôt le matin ou en fin de journée. Mieux vaut prévoir l’essentiel pour rester au sec et au chaud en toutes circonstances.
+                À noter que l’utilisation de parapluies est interdite pendant l'événement afin de ne pas gêner la visibilité des spectateurs.
+            </p>
+
+            <br><br>
+
+        </div>
+
+        <div>
+            <img src="/assets/homePage/articleGuide/pluieImage1.png" alt="image_pluie" class="imagesArticle photoHorizontal"  style="margin-right: 75px;">
+            <img src="/assets/homePage/articleGuide/pluieImage2.png" alt="image_pluie" class="imagesArticle">
+        </div>
+
+        <br><br><br>
+
+        <div class="blocTexte1Container">        
+
+            <p class="blocTexte1">
+
+                <b><span class="listeArticleTitre">2.1 Comment se protéger efficacement de la pluie et du froid ?</span></b>
+
+                <br><br>
+
+                <b>A] <i class="fas fa-cloud-showers-heavy"></i> Un imperméable ou un poncho polyvalent</b>
+
+                <br><br>
+
+                Si vous avez prévu un imperméable léger pour le froid, il fera également l’affaire contre la pluie. 
+                Privilégiez un modèle qui couvre bien le haut du corps et les cuisses. Sinon, pensez à un poncho en plastique, 
+                vendu directement sur place. En cas d’oubli, un sac poubelle de 100 L peut être une solution d’urgence. 
+                Découpez des ouvertures pour la tête et les bras à l’avance, et le tour est joué. Pour le froid, 
+                pensez à porter des couches de vêtements en dessous, comme un pull fin en laine ou une veste polaire qui 
+                restent efficaces même sous un plastique.
+
+                <br><br>
+
+                <b>B] <i class="fas fa-shield-alt"></i> Protégez vos affaires</b>
+
+                <br><br>
+
+                Votre sac peut être imperméable sur de courtes distances, mais une journée entière sous la pluie mettra sa résistance à l’épreuve.
+                Une solution pourrait être de glissez vos affaires dans un sac poubelle avant de les ranger dans votre sac principal. De plus, Les appareils photo et autres équipements 
+                électroniques doivent être particulièrement bien protégés. Emballez-les soigneusement dans des sacs plastiques étanches ou des sacs refermables de type zip.
+
+                <br><br>
+
+                <b>C] <i class="fas fa-umbrella"></i> Gardez vos vêtements et sièges au sec et au chaud</b>
+
+                <br><br>
+                
+                Si le sol ou les sièges sont mouillés à votre arrivée, un sac poubelle peut également servir de protection
+                pour vous asseoir sans vous mouiller. Si le froid est intense, ajoutez un coussin isolant ou une couverture légère pour conserver la chaleur.
+                N’oubliez pas des gants fins mais chauds (en laine ou en polaire), une écharpe multifonction, et un bonnet ou bandeau qui protège les oreilles du vent. 
+                Ce sont des petits éléments qui font une grande différence lorsque la température baisse.
+                
+                <br><br>
+            </p>
+        </div>
+
+        <hr class="hr1">
+
+        <h1 class="sousTitreBloc">3. Logistique pure et simple</h1>
+
+        <div class="blocTexte1Container">
+
+            <p class="blocTexte1">Une journée au circuit demande une organisation minutieuse pour éviter 
+                tout désagrément. Préparer correctement votre sac et planifier vos déplacements sont des étapes 
+                essentielles pour profiter au maximum du Grand Prix.
+            </p>
+
+        </div>
+
+        <br><br>
+
+        <div>
+            <img src="/assets/homePage/articleGuide/logistiqueImage1.jpg" alt="image_pluie" class="imagesArticle photoHorizontal"  style="margin-right: 35px;">
+            <img src="/assets/homePage/articleGuide/logistiqueImage3.jpg" alt="image_pluie" class="imagesArticle photoHorizontal"  style="margin-right: 35px;">
+            <img src="/assets/homePage/articleGuide/logistiqueImage2.jpg" alt="image_pluie" class="imagesArticle">
+        </div>
+
+        <br><br>
+
+        <div class="blocTexte1Container" style="flex-direction: column; align-items: start; gap: 0px;"> 
+
+            <p class="blocTexte1">
+                <b><span class="listeArticleTitre">3.1 Remplir le sac : les indispensables</span></b>
+            </p>
+
+            <br><br>
+
+            <div class="listeObjets" style="text-align: left;">
+                <span class="blocTexte1">🗑️ Prévoir des sacs poubelle multifonctions : <span class="descriptionObjet">Prenez 2 ou 3 sacs poubelle (minimum). 
+                    Ils serviront à protéger vos affaires de la pluie, à garder vos sièges au sec ou encore à ranger des objets fragiles.</span></span>
+
+                    <br><br><hr class="HREntreObjets"><br>
+
+                <span class="blocTexte1">💧 Une bouteille d'eau : <span class="descriptionObjet">Hydratez-vous régulièrement. En plus de boire, 
+                    une bouteille d’eau peut être utilisée pour vous rafraîchir ou nettoyer une petite tâche.</span></span>
+
+                    <br><br><hr class="HREntreObjets"><br>
+
+                <span class="blocTexte1">🦻 Bouchons d’oreilles : <span class="descriptionObjet">Bien qu’ils ne soient pas indispensables pour tout le monde, 
+                    ils sont utiles pour éviter une hypersensibilité due au bruit continu.</span></span>
+
+                    <br><br><hr class="HREntreObjets"><br>
+
+                <span class="blocTexte1">💶 De l'argent liquide : <span class="descriptionObjet">Beaucoup de petits stands ou points de restauration n’acceptent pas les paiements par carte. 
+                    Avoir des espèces sur vous est donc essentiel.</span></span>
+
+                    <br><br><hr class="HREntreObjets"><br>
+
+                <span class="blocTexte1">🎟️ Vos billets : <span class="descriptionObjet">Emportez vos billets physiques ou une version électronique téléchargée localement sur votre smartphone. 
+                    Assurez-vous de les garder à portée de main pour éviter de vous retrouver bloqué à l’entrée</span></span>
+            </div>
+
+            <br><br>
+        
+            <p class="blocTexte1">
+                <b><span class="listeArticleTitre">3.2 Se rendre sur place et gérer son temps</span></b>
+
+                <br><br>
+
+                <b>A] <i class="fas fa-clock"></i> Arriver tôt et gérer les files d'attentes</b>
+
+                <br><br>
+
+                Anticipez les embouteillages, les fermetures de routes ou les foules. Partir en avance vous permettra d’éviter 
+                les imprévus et de passer sereinement les contrôles de sécurité. De plus, que ce soit à l'entrée de l'événement ou des gradins, les contrôles de tickets et de sécurité peuvent être longs, 
+                surtout avant les séances principales. Arriver en avance vous évitera de manquer des moments clés.
+
+                <br><br>
+
+                <b>B] <i class="fas fa-ticket-alt"></i> Gardez vos billets en sécurité</b>
+
+                <br><br>
+
+                Ne jetez jamais vos billets ou contre-marques avant la fin de l’événement. Ils pourraient être nécessaires pour rentrer après une pause ou accéder à certaines zones. 
+                Conservez-les précieusement, car ils pourraient aussi devenir de précieux souvenirs de votre expérience sur le circuit.
+
+                <br><br>
+
+                <b>C] <i class="fas fa-coffee"></i> Optimisez vos pauses</b>
+
+                <br><br>
+
+                Profitez des moments creux pour aller aux toilettes, même si ce n’est pas urgent. Cela vous évitera de longues files d’attente pendant les temps forts. 
+                De plus, cela vous permettra de revenir à votre place sans stress avant le début des moments clés.
+            </p>
+        </div>
+
+        <hr class="hr1">
+
+        <h1 class="sousTitreBloc">4. Technique et technologie</h1>
+
+        <div class="blocTexte1Container">
+
+            <p class="blocTexte1">Assister à un Grand Prix est avant tout une expérience visuelle et sonore. 
+                Cependant, si vous souhaitez immortaliser ces moments avec des photos et vidéos de qualité, 
+                cela demande un minimum de préparation. Bien que non indispensable pour profiter de l’événement, cette section 
+                s’adresse à ceux qui veulent optimiser leur matériel et leur technique pour des clichés mémorables.
+            </p>
+
+        </div>
+
+        <br><br>
+
+        <div>
+            <img src="/assets/homePage/articleGuide/techniqueImage1.jpg" alt="image_pluie" class="imagesArticle photoHorizontal"  style="margin-right: 70px;">
+            <img src="/assets/homePage/articleGuide/techniqueImage2.jpg" alt="image_pluie" class="imagesArticle photoHorizontal">
+        </div>
+
+        <br><br>
+
+        <div class="blocTexte1Container">    
+
+            <p class="blocTexte1">
+
+                <b><span class="listeArticleTitre">4.1 Photo et vidéo : quel matériel emporter ?</span></b>
+
+                <br><br>
+
+                <b>A] <i class="fas fa-camera"></i> Le matériel idéal pour les passionnés</b>
+
+                <br><br>
+
+                Si vous possédez un reflex, privilégiez deux objectifs : <b>24-105 mm</b>, idéal pour les photos dans le village F1 ou les stands. 
+                <b>70-200 mm ou 70-300 mm</b>, parfait pour capturer des détails ou des voitures en mouvement sur la piste. Cependant, si vous cherchez une 
+                solution différente, les GoPro et autres caméras d’action sont idéales pour capturer l’ambiance ou filmer vos déplacements, mais leur grand 
+                angle limite leur efficacité pour photographier la piste.
+
+                <br><br>
+
+                <b>B] <i class="fas fa-mobile-alt"></i> Smartphone : pour la simplicité</b>
+
+                <br><br>
+
+                Les smartphones modernes offrent de belles performances, mais évitez de zoomer lors de la prise de vue, car cela réduit la qualité. 
+                Prenez des clichés sans zoom, puis recadrez-les avec un logiciel plus tard. N'hésitez pas à installez des applications qui permettent de 
+                photographier en RAW ou de régler manuellement des paramètres comme la vitesse d’obturation et l’exposition.
+
+                <br><br>
+
+                <b>C] <i class="fas fa-tools"></i> Accessoires pratiques</b>
+
+                <br><br>
+
+                Le <b>monopod</b> : Léger, compact et adapté aux espaces restreints des tribunes, un monopod est idéal pour stabiliser vos prises de vue. <br>
+                <b>Batteries supplémentaires</b> : Une batterie ne suffira pas si vous manipulez fréquemment l’écran ou filmez. Prévoyez-en au moins une ou deux supplémentaires. <br>
+                <b>Cartes mémoire</b>: Emportez plusieurs cartes mémoire de grande capacité (au moins 64 Go), surtout si vous photographiez en RAW ou filmez en haute définition (1080p ou 4K).
+
+                <br><br><br>
+
+                <b><span class="listeArticleTitre">4.2 Technique : conseils pour des clichés réussis</span></b>
+
+                <br><br>
+
+                <b>A] <i class="fas fa-dumbbell"></i> Entraînez-vous avant l’événement</b>
+
+                <br><br>
+
+                Testez votre matériel avec des voitures en mouvement près de chez vous. 
+                Cela vous permettra d’ajuster vos réglages et de vous familiariser avec les contraintes. 
+                Entraînez-vous également à suivre un véhicule en mouvement avec fluidité
+
+                <br><br>
+
+                <b>B] <i class="fas fa-cogs"></i> Réglez votre appareil en mode manuel</b>
+
+                <br><br>
+
+                Pour de meilleurs résultats, réglez votre appareil en mode manuel avec une vitesse d’obturation rapide (1/1000 ou plus) pour figer les voitures en mouvement. 
+                Adaptez les ISO selon la lumière (400 par beau temps, 800 sous un ciel couvert) et utilisez le format RAW pour faciliter le post-traitement.
+
+                <br><br>
+
+                <b>C] <i class="fas fa-level-up-alt"></i> Stabilisation</b>
+
+                <br><br>
+
+                Si votre téléobjectif propose plusieurs modes, choisissez la stabilisation horizontale (mode II) pour suivre les voitures en mouvement. 
+                Cela permettra de minimiser les vibrations lorsque vous effectuez des panoramiques, garantissant des images nettes tout en capturant le mouvement.
+            </p>
+        </div>
+
+        <hr class="hr1">
         
     </div>
 
@@ -15,7 +373,9 @@
 </script>
 
 
+<!-- ========================================================================================== -->
+<!-- ===================================== BALISE STYLE  ====================================== -->
+<!-- ========================================================================================== -->
 
-<style>
 
-</style>
+<style src="../styles/ArticlesPages.css"></style>

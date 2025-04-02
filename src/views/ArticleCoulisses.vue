a7266e3bb4f8d58a4b48d5e07cecb637adc1de76
VUILLIN Thomas <144356846+vuillin@users.noreply.github.com>
2025-04-02 19:52:24 +0200

    nouvel article

--- src/views/ArticleCoulisses.vue
+++ src/views/ArticleCoulisses.vue
@@ -3,6 +3,187 @@
     <div>
 
         <h1 class="titreArticle">Les coulisses de l'événement</h1>
+
+        <p class="sousTitreArticle">Le Quantic Grand Prix, 
+            cache derrière ses tribunes et son glamour une véritable mécanique humaine complexe et passionnante. 
+            Derréglée, celle-ci pourrait compromettre toute l'expérience. Découvrons ensemble les hommes et les femmes 
+            qui œuvrent discrètement mais efficacement dans l'ombre de la compétition, et sans qui rien ne serait possible.
+
+            <br> <br>
+
+        </p>
+
+        <p class="sousTitreArticlebis">🗞️ - Article (4 minutes de lecture)</p>
+
+        <hr class="hr1">
+
+
+        <div class="blocTexte1Container">
+
+            <img src="/assets/homePage/semih.png" alt="ImageUne" class="imagesArticleCoulisses">
+
+            <p class="blocTexte1">
+
+                <span class="nom_staff">Semih Arshlan - Journaliste 📰</span>
+
+                <br> <br> <br>
+                
+                L'organisation d'un événement de l'ampleur du Quantic Grand Prix représente un défi logistique colossal, mobilisant une multitude de professionnels aux compétences variées. Parmi eux, 
+                les journalistes jouent un rôle clé en relayant l'effervescence de la course au grand public.
+
+                <br> <br>
+
+                Semih Arshlan, reporter chevronné couvrant le sport automobile depuis plusieurs années, partage son expérience : 
+                « Le Quantic Grand Prix n'est pas seulement une compétition, c'est une véritable occassion de rassembler tous les passionnées
+                de Formule 1 et plus largement d'automobile. »
+                Présent dès les premières lueurs du jour sur le circuit, Semih assiste aux préparatifs des écuries, observe les ingénieurs peaufiner les réglages 
+                et échange avec les pilotes pour saisir leurs impressions avant la course.
+
+                <br> <br>
+
+                « Être au cœur de l'action me permet de fournir des analyses précises et des anecdotes inédites 
+                qui plongent nos lecteurs dans les coulisses de l'événement », souligne-t-il. Cette immersion totale 
+                est essentielle pour offrir une couverture médiatique à la hauteur de l'événement.
+            </p>
+
+        </div>
+
+        <br> 
+        <hr class="hr1">
+        <br>
+
+        <div class="blocTexte1Container">
+
+            <img src="/assets/homePage/esteban.png" alt="ImageUne" class="imagesArticleCoulisses">
+
+            <p class="blocTexte1">
+
+                <span class="nom_staff">Esteban Barta-Malo - Spectateur 🏁</span>
+
+                <br> <br> <br>
+                
+                Assister au Quantic Grand Prix est une expérience inoubliable pour tout passionné de sport automobile. 
+                Esteban, fervent amateur de Formule 1, partage son ressenti
+
+                <br> <br>
+
+                « Être présent au Quantic Grand Prix, c'est vivre une ambiance électrique et ressentir l'adrénaline des courses en direct. » 
+                Pour Esteban, la préparation est essentielle : « J'ai réservé mes billets et mon hébergement plusieurs mois à 
+                l'avance pour être sûr d'avoir une bonne place et éviter les mauvaises surprises. »
+
+                <br> <br>
+
+                Le jour de la course, il arrive tôt pour s'imprégner de l'atmosphère et profiter des animations pré-événement. 
+                « Voir les équipes se préparer, entendre le rugissement des moteurs qui s'échauffent, c'est une montée d'excitation 
+                indescriptible. »
+            </p>
+
+        </div>
+
+        <br> 
+        <hr class="hr1">
+        <br>
+
+        <div class="blocTexte1Container">
+
+            <img src="/assets/homePage/matteo.png" alt="ImageUne" class="imagesArticleCoulisses">
+
+            <p class="blocTexte1">
+
+                <span class="nom_staff">Mattéo Veaulin - Ingénieur F1 ⚙️</span>
+
+                <br> <br> <br>
+                
+                Les ingénieurs de course jouent un rôle essentiel dans la performance des monoplaces. 
+                Mattéo Veaulin, ingénieur de course expérimenté, décrit son quotidien.
+
+                <br> <br>
+
+                « Mon rôle est d'être le principal point de contact entre le pilote et l'équipe technique. 
+                Je traduis les retours du pilote en ajustements techniques pour optimiser la voiture. » 
+                Pendant la course, Mattéo surveille en temps réel les données transmises par la monoplace, 
+                telles que la vitesse, la température des pneus et la consommation de carburant, afin de prendre des décisions stratégiques cruciales.
+
+                <br> <br>
+
+                « Chaque décision peut influencer le résultat de la course. Il est impératif d'analyser rapidement les informations et
+                de communiquer efficacement avec le pilote », souligne-t-il. Cette collaboration étroite entre l'ingénieur et le pilote
+                est déterminante pour maximiser les performances et anticiper les imprévus en piste.
+            </p>
+
+        </div>
+
+        <br> 
+        <hr class="hr1">
+        <br>
+
+        <div class="blocTexte1Container">
+
+            <img src="/assets/homePage/ibrahim.png" alt="ImageUne" class="imagesArticleCoulisses">
+
+            <p class="blocTexte1">
+
+                <span class="nom_staff">Ibrahim Batal - Directeur de course 📌</span>
+
+                <br> <br> <br>
+                
+                Le directeur de course joue un rôle essentiel pour assurer le bon déroulement et la sécurité de l'événement.
+                Ibrahim Batal, occupant cette fonction, décrit ses responsabilités
+
+                <br> <br>
+
+                « Mon objectif principal est de veiller à ce que chaque aspect de la course se déroule conformément aux réglementations, 
+                tout en garantissant la sécurité des pilotes, des équipes et des spectateurs. » Dès les premières heures de la journée, 
+                Ibrahim supervise les inspections de la piste, coordonne les équipes de commissaires et s'assure que tous les dispositifs 
+                de sécurité sont opérationnels.
+
+                <br> <br>
+
+                Pendant la course, il est en communication constante avec les équipes et les officiels pour gérer les incidents en temps réel. 
+                « Chaque décision doit être prise rapidement et avec précision, car elles peuvent avoir un impact significatif sur le déroulement 
+                de l'épreuve », souligne-t-il. Grâce à son expertise et à sa vigilance, Ibrahim garantit une compétition équitable et sécurisée 
+                pour tous les participants.
+            </p>
+
+        </div>
+
+        <br> 
+        <hr class="hr1">
+        <br>
+
+        <div class="blocTexte1Container">
+
+            <img src="/assets/homePage/thomas.png" alt="ImageUne" class="imagesArticleCoulisses">
+
+            <p class="blocTexte1">
+
+                <span class="nom_staff">Thomas Vilain - Pilote 🏎️</span>
+
+                <br> <br> <br>
+                
+                Au cœur de l'action du Quantic Grand Prix, les pilotes incarnent la passion et la détermination propres à la Formule 1. 
+                Thomas Vilain, pilote chevronné, partage son expérience : « Participer au Quantic Grand Prix est toujours un moment fort de la saison. 
+                L'atmosphère unique et le tracé exigeant du circuit en font un défi intéressant. »
+
+                <br> <br>
+
+                Les journées de Thomas commencent bien avant le jour de la course, avec des séances intensives de préparation physique et mentale. 
+                « Chaque détail compte, de la condition physique à la compréhension technique de la voiture. 
+                Il est essentiel d'être en parfaite harmonie avec l'équipe d'ingénieurs pour optimiser les performances. »
+
+                <br> <br>
+
+                Pendant la course, la concentration est maximale. « À ces vitesses, il n'y a pas de place pour l'erreur. 
+                Chaque décision doit être prise en une fraction de seconde, en tenant compte des informations transmises par 
+                l'équipe et des conditions de course. » Cette combinaison de préparation rigoureuse, de collaboration étroite 
+                avec l'équipe et de passion pour la course est essentielle pour performer lors du Grand Prix.
+            </p>
+
+        </div>
+
+        <br> 
+        <hr class="hr1">
+        <br>
         
     </div>
 
